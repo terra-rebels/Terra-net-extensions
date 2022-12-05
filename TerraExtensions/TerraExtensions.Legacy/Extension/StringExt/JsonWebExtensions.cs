c89460c1ad4f14b7f59e039c25d40414bab46f72
Dan Gerchcovich <dan.developer789@gmail.com>
2022-12-06 01:13:17 +1100

    Legacy Components

---
+++ TerraExtensions/TerraExtensions.Legacy/Extension/StringExt/JsonWebExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace TerraNetExtensions.StringExt
+{
+    public static class JsonWebExtensions
+    {
+        public static string RemoveNull(this string json)
+        {
+            if (!string.IsNullOrWhiteSpace(json))
+            {
+                StringBuilder jsonFormatter = new StringBuilder();
+                jsonFormatter.Append(json);
+                jsonFormatter.Replace("\\", string.Empty);
+                return jsonFormatter.ToString().Trim('"');
+            }
+            else
+                return json;
+        }
+    }
+}
--- TerraExtensions/TerraExtensions/Extension/IntExt/IntegerExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TerraNetExtensions.IntExt
-{
-    public static class IntegerExtensions
-    {
-        public static byte[] ConvertFromIntArray(this Int32[] data) => data.SelectMany(BitConverter.GetBytes).ToArray();
-        public static Int32[] ConvertToInt32Array(this byte[] data) => data.Select(w => Convert.ToInt32(w)).ToArray();
-    }
-}
